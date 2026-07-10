1533c651cf6155a8d32d21403765af7c8a6a283a
Dany <95555380+adanydev@users.noreply.github.com>
2026-07-09 22:16:32 -0400

    Created menu for UI

--- FarmBooks.UI/MainWindow.xaml.cs
+++ FarmBooks.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using FarmBooks.UI.ViewModels;
 using FarmBooks.UI.Views;
 
 namespace FarmBooks.UI;
@@ -16,15 +18,51 @@ public partial class MainWindow : Window
         _accountingCodesView = accountingCodesView;
 
         MainContent.Content = _expensesView;
+
+        CommandBindings.Add(
+            new CommandBinding(ApplicationCommands.New, (_, _) => OpenNewExpense())
+        );
+
+        InputBindings.Add(
+            new KeyBinding(ApplicationCommands.New, new KeyGesture(Key.N, ModifierKeys.Control))
+        );
     }
 
-    private void ExpensesButton_Click(object sender, RoutedEventArgs e)
+    private void ExpensesMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        ShowExpenses();
+    }
+
+    private void AccountingCodesMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        MainContent.Content = _accountingCodesView;
+    }
+
+    private void NewExpenseMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        OpenNewExpense();
+    }
+
+    private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void ShowExpenses()
     {
         MainContent.Content = _expensesView;
     }
 
-    private void AccountingCodesButton_Click(object sender, RoutedEventArgs e)
+    private void OpenNewExpense()
     {
-        MainContent.Content = _accountingCodesView;
+        ShowExpenses();
+
+        if (
+            _expensesView.DataContext is ExpensesViewModel viewModel
+            && viewModel.NewExpenseCommand.CanExecute(null)
+        )
+        {
+            viewModel.NewExpenseCommand.Execute(null);
+        }
     }
 }
